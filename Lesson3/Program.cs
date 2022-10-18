631177de7fe1eb5eeb8237248cc5233aac72df2e
Viktoryia Supronenko <vika.supronenko@icloud.com>
2022-10-18 18:03:10 +0300

    Add project files.

---
+++ Lesson3/Program.cs
@@ -0,0 +1,70 @@
+﻿using Lesson3;
+
+double num1, num2;
+double result = 0;
+char operation;
+
+while (true)
+{
+    Console.Clear();
+
+    try
+    {
+        Console.Write("Enter the first number: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter the second number: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("The number is incorrect");
+        Console.ReadLine();
+        continue;
+    }
+
+    Console.WriteLine("Enter operation: ");
+    operation = Convert.ToChar(Console.ReadLine());
+
+    switch (operation)
+    {
+        case '+':
+            result = num1 + num2;
+            Console.WriteLine("Operation result: " + result);
+            break;
+        case '-':
+            result = num1 - num2;
+            Console.WriteLine("Operation result: " + result);
+            break;
+        case '*':
+            result = num1 * num2;
+            Console.WriteLine("Operation result: " + result);
+            break;
+        case '/':
+            if (num2 == 0)
+            {
+                Console.WriteLine("Divisor cannot be zero");
+                continue;
+            }
+            result = num1 / num2;
+            Console.WriteLine("Operation result: " + result);
+            break;
+        default:
+            Console.WriteLine("Invalid action. Try again.");
+            break;
+    }
+
+
+    QueueRes res = new QueueRes();
+    res.AddToQueue(result);
+    res.LastResults();
+    Console.ReadLine();
+}
+
+
+
+
+
+
+
+
+
---
+++ Lesson3/QueueRes.cs
@@ -0,0 +1,29 @@
+﻿namespace Lesson3
+{
+    internal class QueueRes
+    {
+        static public Queue<double> lastResults = new Queue<double>();
+        public double AddToQueue(double res)
+        {
+            var input = res;
+            lastResults.Enqueue(input);
+            if (lastResults.Count > 5)
+            {
+                lastResults.Dequeue();
+            }
+            return input;
+        }
+
+        public void LastResults()
+        {
+            Console.WriteLine("History of last 5 results: ");
+            int num = 0;
+            foreach (var item in lastResults)
+            {
+                ++num;
+                Console.WriteLine($"Result {num}:  {item}");
+            }
+        }
+    }
+}
+
